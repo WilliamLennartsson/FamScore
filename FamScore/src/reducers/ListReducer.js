2d8bfbbe0aea9dd6563f6229676034fb5de2a13b
KentAgent <georgios.apostolidis92@gmail.com>
2018-08-20 10:50:07 +0200

    Added redux functionality for ToDoList. Statehandling from store

--- FamScore/src/reducers/ListReducer.js
+++ FamScore/src/reducers/ListReducer.js
@@ -1,18 +1,29 @@
-import { 
+import {
     DELETE_ITEM
 } from '../actions/types'
 
-const INITIAL_STATE = { 
-    list: []
+const INITIAL_STATE = {
+    list: [
+        {
+            points: 50,
+            titleText: 'Tjena',
+            infoText: 'gahafgadfgagragra'
+        },
+        {
+            points: 100,
+            titleText: 'Hallå',
+            infoText: 'grghhthtafeefefe'
+        }
+    ]
 }
 
 export default (state = INITIAL_STATE, action) => {
     console.log(action);
     switch (action.type) {
         case DELETE_ITEM:
-            return { 
-                ...state, 
-                list: action.payload 
+            return {
+                ...state,
+                list: action.payload
             }
         default:
             return state
--- FamScore/src/reducers/index.js
+++ FamScore/src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux'
 import ListReducer from './ListReducer'
 
 export default combineReducers({
-    auth: ListReducer
+    listReducer: ListReducer
 })
