bf4b26ec42d7c919b7e5582e2bd9dfd6c7df9fc3
KentAgent <georgios.apostolidis92@gmail.com>
2018-08-22 10:51:44 +0200

    Added isAdmin functionality.

--- Frontend/src/actions/types.js
+++ Frontend/src/actions/types.js
@@ -1,5 +1,10 @@
 // We declare types here so we avoid any typos
 
+//ListReducer
 export const DELETE_ITEM = 'DELETE ITEM'
 export const SHOW_ADD_MISSION = 'SHOW_ADD_MISSION'
 export const ADD_MISSION = 'ADD_MISSION'
+
+//RewardsReducer
+export const CHECK_IF_ADMIN = 'CHECK_IF_ADMIN'
+export const SET_ADMIN = 'SET_ADMIN'
